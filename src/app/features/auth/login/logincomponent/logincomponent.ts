ad36167efcadbd37e433e08d993d60d071834622
hillarys camilo <hcr090120@gmail.com>
2026-08-19 17:40:48 -0400

    cambio

--- src/app/features/auth/login/logincomponent/logincomponent.ts
+++ src/app/features/auth/login/logincomponent/logincomponent.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../../auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-logincomponent',
-  imports: [],
+  imports: [ReactiveFormsModule],
   standalone: true,
   templateUrl: './logincomponent.html',
   styleUrl: './logincomponent.css',
 })
 export class Logincomponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(Auth);
+  private router = inject(Router);
 
+  loginForm = this.fb.nonNullable.group({
+    email: this.fb.control('', [Validators.required, Validators.email]),
+    password: this.fb.control('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  get email() {
+    return this.loginForm.get('email')!;
+  }
+
+  get password() {
+    return this.loginForm.get('password')!;
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: () => {
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          console.error('Login failed', err);
+        },
+      });
+
+      console.log('Datos del formulario:', this.loginForm.getRawValue());
+      this.authService.login(email, password).subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+        },
+      });
+    }
+  }
 }
