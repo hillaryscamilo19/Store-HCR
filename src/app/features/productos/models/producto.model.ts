14be42e18b3da742d82fbd796677d1c92af71e8c
hillarys camilo <hcr090120@gmail.com>
2026-08-26 17:22:21 -0400

    cambio

---
+++ src/app/features/productos/models/producto.model.ts
@@ -0,0 +1,13 @@
+export interface Producto {
+    id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  stock: number;
+  brand: string;
+  category: string;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
