6a0fc617ca15e488f2d7dcb8fc2a02b57a27088f
hillarys camilo <hcr090120@gmail.com>
2026-08-31 17:59:31 -0400

    cambio

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./features/home/home";
 import { Products } from "./features/products/products";
+import { Footer } from "./layout/footer/footer/footer";
+import { Navbar } from "./layout/navbar/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Products],
+  imports: [RouterOutlet, Home, Products, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/shared/components/product-card/product-card.ts
+++ src/app/shared/components/product-card/product-card.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Producto } from '../../../features/productos/models/producto.model';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  standalone: true,
+  imports: [DecimalPipe],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
 export class ProductCard {
+  producto = input.required<Producto>();
+  verProducto = output<number>();
+  agregarCarrito = output<Producto>();
 
+  onVerProducto(): void {
+    this.verProducto.emit(this.producto().id);
+  }
+
+  onAgregarCarrito(): void {
+    this.agregarCarrito.emit(this.producto());
+  }
 }
